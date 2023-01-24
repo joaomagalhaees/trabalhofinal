bac9febb1822d2f46006487df7159fa9236853f6
joaomagalhaees <115542752+joaomagalhaees@users.noreply.github.com>
2023-01-24 23:09:38 +0000

    Add files via upload

---
+++ pagamento.js
@@ -0,0 +1,18 @@
+function navbar() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  
+  function pagamento() {
+    var numCartao = document.getElementById("num-cartao").value;
+    var nomeCartao = document.getElementById("nome-cartao").value;
+    var valCartao = document.getElementById("val-cartao").value;
+    var cvvCartao = document.getElementById("cvv-cartao").value;
+    if (numCartao != "" && nomeCartao != "" && valCartao != "" && cvvCartao != "") {
+      alert("Pagamento efetuado com sucesso!")
+    }
+  }
